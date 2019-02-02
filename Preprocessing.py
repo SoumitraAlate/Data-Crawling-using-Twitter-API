e2f2a32ce00c3f948513cf52711979d4b1b2b948
Soumitra Alate <46055615+SoumitraAlate@users.noreply.github.com>
2019-02-02 11:07:25 -0500

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,3 +1,13 @@
+#CSE 535 - Information Retrieval - Project 1 : Data Crawling using Twitter API
+
+'''
+A simple python script to crawl data using the streaming API of twitter and classify it into seperate files such as 
+City, Text, Emoticons, Hashtags, topic in the text, Language used in tweet
+
+@author Soumitra Alate'''
+
+
+#Tweepy tool is used to access the Twitter Data
 from typing import List, Any, Union
 from nltk.corpus import stopwords
 import tweepy
@@ -13,53 +23,60 @@ import time
 from nltk.corpus import stopwords
 import random
 
+#Variables that contains the user credentials to access the twitter API
 consumer_key = "FuG9MVZ4I5mmZENb5rTyplhoF"
 consumer_secret = "PUbx3R2eWNRbYaHReeMmpK8xuIiJWShoNu2Mk0sDaye36xRvqc"
 access_token = "1037053024617865216-VUcTGKqWP4kZugGXuJg7f8byadgZk9"
 access_secret = "IZerP1C6gbGoL4t2uPUbpHVxwti5mhqJTf82cACuTYmcu"
 
 
+#Using OAuth interface to allow the app to access the twitter data
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
+#Creating the API object while passing the auth information
 api = tweepy.API(auth)
 
+#Using the streamListner API to crawl the data
 class MyListener(StreamListener):
 
     def on_data(self, data):
 
         try:
-            politics = ["ศาลสูง", "ศาลสูง", "donald trump twitter", "trump twitter", "legislative", "เทศบาล",
-                        "การเลือกตั้ง", "ภาษี", "ภาษี", "federalism", "ตุลาการ", " กฎหมาย ", " รัฐบาล ", " รัฐมนตรี ",
-                        " การเมือง ", " คนดี ", " โดนัลด์ทรัมพ์ ", " นักการเมือง ", " โอบามา ", " การเมืองสด ", " น",
-                        " นายกรัฐมนตรี " "พรรคประชาธิปัตย์", "pehu thai", "bhumaji thai", "การเลือกตั้ง",
-                        "ประธานาธิบดี", "prayut chan-o-cha", "chan-o-cha", "พรรคเดโมแครต", "Ṣ̄āls̄ūng", "ṣ̄āls̄ūng",
-                        "donald trump twitter", "trump twitter", "legislative", "theṣ̄bāl", "kār leụ̄xktậng",
-                        "p̣hās̄ʹī", "p̣hās̄ʹī", "federalism", "tulākār", " kḍh̄māy", " rạṭ̄hbāl", " rạṭ̄hmntrī",
-                        " kārmeụ̄xng", " khndī", " donạ ld̒ thrạmph̒ ", " nạkkārmeụ̄xng", " xo bā mā",
-                        " kārmeụ̄xng s̄d", " n. ", " Nāykrạṭ̄hmntrī" "phrrkh prachāṭhipạty̒", "pehu thai",
-                        "bhumaji thai", "kār leụ̄xktậng", "praṭhānāṭhibdī", "prayut chan-o-cha", "chan-o-cha",
-                        "phrrkh de mo khært"]
-            crime = ["ข่มขืน", "ฆาตกร", "ฆาตกร", "ฆาตกร", "ฆาตกรรม", "การรุกราน", "สมรู้ร่วมคิด", "ฆ่า", "ฆ่า",
-                     "โจรกรรม", "อาชญากรรม", "ทำร้ายร่างกาย", "หยื่อปืน", "ผิด", "ฆาตกรรม", "ลักพาตัว", "ยา", "ศาล",
-                     "K̄h̀mk̄hụ̄n", "ḳhātkr", "ḳhātkr", "ḳhātkr", "ḳhātkrrm", "kār rukrān", "s̄mrū̂ r̀wm khid", "ḳh̀ā",
-                     "ḳh̀ā", "corkrrm", "xāchỵākrrm", "thảr̂āy r̀āngkāy","ṣ̄āl"]
-            socialUnrest = ["การประท้วง", "การจลาจล", "การจลาจล", "จัดระเบียบ", "ประชาธิปไตย", "ประชาธิปไตย",
-                            "ความขัดแย้ง", "การปฏิวัติ", "วิกฤต", "Kār pratĥwng", "kār clācl", "kār clācl",
-                            "cạd rabeīyb", "prachāṭhiptịy", "prachāṭhiptịy", "khwām k̄hạdyæ̂ng", "kār pt̩iwạti",
-                            "wikvt"]
-            envi = ["อากาศคุณภาพ", "ดัชนีคุณภาพอากาศด้านสุขภาพ", "ความร้อน", "น้ำท่วม", "ภัยแล้ง", "พายุฝุ่น",
-                           "หมอกควัน", "มลพิษ", "สายฝน", "สิ่งแวดล้อม", "การเปลี่ยนแปลงสภาพภูมิอากาศ" "สภาพอากาศ",
-                           "Xākāṣ̄ khuṇp̣hāph", "dạchnī khuṇp̣hāph xākāṣ̄ d̂ān s̄uk̄hp̣hāph", "khwām r̂xn", "n̂ả th̀wm",
-                           "p̣hạy læ̂ng", "phāyu f̄ùn", "h̄mxk khwạn", "mlphis", "s̄āy f̄n", "s̄ìngwædl̂xm",
-                           "kār pelī̀ynpælng s̄p̣hāph p̣hūmi xākāṣ̄", "s̄p̣hāph xākāṣ̄"]
-            infrastructure = ["ถนน", "น้ำมัน", "สนามบิน",
-                              "พลังงาน" "น้ำ" "สุขาภิบาล" "ท่องเที่ยว" "สุขภาพ" "การศึกษา" "พลังงาน" "โรงเรียน",
-                              "วิทยาลัย", "การขนส่ง ", 'การจราจร', 'สถานบันเทิงยามค่ำคืน', 'อาคาร', 'หลุมบ่อ', 'สร้าง',
-                              'การก่อสร้าง', "T̄hnn", "n̂ảmạn", "s̄nām bin",
-                              "phlạngngān" "n̂ả" "s̄uk̄hāp̣hibāl" "th̀xngtheī̀yw" "s̄uk̄hp̣hāph" "kār ṣ̄ụks̄ʹā" "phlạngngān" "rongreīyn",
-                              "withyālạy", "kār k̄hns̄̀ng", "kār crācr", "s̄t̄hān bạntheing yām kh̀ảkhụ̄n", "xākhār",
-                              "h̄lum b̀x", "s̄r̂āng", "kār k̀xs̄r̂āng"]
+            politics = ["#trump", "#modi", "#obama", "#donaldtrump", "#hilaryclinton","#politics", "governs", "governor", "donald trump", "trump", "legislative", "municipal", "washington dc",
+                        "election", "tax", "taxes", "whitehouse", "federalism", "judicial", "laws", " bill de blasio",
+                        "politics", "congress", "politicians", "obama", "hilary clinton", "politics live", "democrat",
+                        "republican", "modi", "polls", "elections", "democrats", "republicans", "democratic", "senator",
+                        "president","prime minister", "political", "government"]
+            
+            crime = ["#threats","#rape","#crime","#voilent","accused", "brutality", "blackmail", "jail", "raid", "zeal", "zealous", "threat", "threatening",
+                     "threats",
+                     "fight", "violence", "violent", "guilty", "cheat", "capture", "damage", "crime", "crimes",
+                     "custody",
+                     "speeding", "kills", "harass", "harassed", "harassment", "arrested", "arrest", "stalk", "stalking",
+                     "raped",
+                     "rape", "murderers", "muggers", "mugging", "collusion", "kill", "abuse", "robbery", "felony",
+                     "assault",
+                     "burglary", "grand larceny", "victim", "gun", "culprit", "murder", "kidnapping", "drugs", "police",
+                     "traffic lights", "court", "911"]
+            
+            socialUnrest = ["social unrest", "unrest", "violent protest", "radical", "radicalized", "strikes",
+                            "protests",
+                            "riots", "march", "organize", "democracia", "conflicto", "revolucion", "criminalidade",
+                            "crisis", "disorder", "unease", "upheaval", "distress", "anarchy", "turmoil"]
+            
+            envi = ["#airquality", "#floods", "#droughts", "#duststorms", "#storm", "#rain","#florence","#hurricane", "smog","drizzle", "light shower", "fine rain","windy", "cloud", "planet earth", "wildlife", "al gore", "solar", "radiation", "forests", "oxygen",
+                    "forest", "bush", "plantation", "plants", "soil", "fog", "whirlwind", "windstorm"
+                    "erosion", "hydrocarbon", "hydrocarbons", "biodegradable", "biodiversity", "carbon dioxide",
+                    "environmental", "sustainability", "environment", "florence", "hurricane", "storm", "thunderstorm",
+                    "smoke", "weather", "air quality", "air quality health index", "heat", "flood", "emission",
+                    "emissions",
+                    "extinction", "biohazard", "ecosystem", "drought", "dust", "storm", "smog", "pollution", "humid",
+                    "rainfall", "rain", "cloudy", "environment", "climatechange"]
+            
+            infrastructure = ["sewage", "infrastructure", "infra", "jobs", "job", "roads", "petrol", "power", "water",
+                              "sanitation", "tourism", "health", "education", "energy", "transportation", "traffic",
+                              "night life", "buildings", "potholes", "constructed", "construction"]
 
             a = {}
             b = {}
@@ -69,23 +86,28 @@ class MyListener(StreamListener):
             z = {}
             y = {}
             h = {}
-            file = open("Bangkok100.json", 'a', encoding='utf-8')
+            file = open("NYC.json", 'a', encoding='utf-8')
             tweetData = json.loads(data)
             text_en1 = tweetData["text"]
+            
+            #Filtering out the retweets
             if not tweetData["retweeted"] and 'RT @' not in text_en1 and 'th' in tweetData["lang"]:
 
+                #Crawling the HashTags
                 hash1 = re.findall(r"#(\w+)", text_en1)
                 h = {'hashtags': hash1}
 
-                foo = ['politics']
+                foo = ['politics','crime','socialUnrest','envi','infrastructure']
                 ran = random.choice(foo)
 
-                # if any(topics in text_en1 for topics in envi):
-                #     a = {"topic": "environment"}
-                if any(topics in text_en1 for topics in politics):
-                    a = {"topic": "politics"}
-                # elif any(topics in text_en1 for topics in crime):
-                #     a = {"topic": "crime"}
+                if any(topics in text_en1 for topics in envi):
+                     a = {"topic": "environment"}
+                elif any(topics in text_en1 for topics in politics):
+                     a = {"topic": "politics"}
+                elif any(topics in text_en1 for topics in socialUnrest):
+                     a = {"topic": "socialunrest"}
+                elif any(topics in text_en1 for topics in crime):
+                     a = {"topic": "crime"}
                 else:
                     a = {"topic": ran}
 
@@ -102,16 +124,20 @@ class MyListener(StreamListener):
                 result = re.findall("@([a-zA-Z0-9]{1,15})", text_en1)
 
                 m = {'mentions': result}
+                
+                b = {'city': 'NYC'}
+                
+                #Tweet Location : NYC 
+                d = {'tweet_loc': '40.715886, -74.005087'}
 
-                b = {'city': 'Bangkok'}
-                d = {'tweet_loc': '13.752224, 100.508611'}
-
+                #Regex to crawl the emoticons from twitter                
                 text = ' '.join(re.sub("(@[A-Za-z0-9]+)|(#[A-Za-z0-9]+)|(:D)|([^0-9A-Za-zぁ-んァ-ン一-龥っつ゜ニノ三二])|"
                                        "([^0-9A-Za-zぁ-んァ-ン一-龥ovっつ゜ニノ三二])|"
                                        "([^0-9A-Za-zぁ-んァ-ン一-龥ｦ-ﾟ\)∩　）])|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", text_en1).split())
 
                 text1 = ' '.join([word for word in text.split() if word not in (stopwords.words('english'))])
 
+                #Checking for tweets in various languages : English, Hindi, French, Spanish, Thai
                 if tweetData['lang'] == 'en':
                     z = {'text_en': text1}
                 elif tweetData['lang'] == 'hi':
@@ -130,9 +156,8 @@ class MyListener(StreamListener):
                 file.write(data.append(str(b)))
                 # file.write(str(b))
                 file.write(data.append(str(y)))
-                # file.write(str(y))
                 file.write(data.append(str(d)))
-                # file.write(str(d))
+               
 
                 # json.dump(e, file, ensure_ascii=False)
                 file.write((data.append(str(m))))
@@ -155,5 +180,5 @@ class MyListener(StreamListener):
 
 twitter_stream = Stream(auth, MyListener())
 
-# Bangkok
-twitter_stream.filter(locations=[100.3279, 13.2167, 100.9384, 13.9552])
+twitter_stream.filter(track=['NYC', 'New York City', 'Manhattan', 'brooklyn', 'Times Square', 'Central Park',
+                             'Empire State Building', 'Statue of Liberty', 'queens', 'bronx'])
